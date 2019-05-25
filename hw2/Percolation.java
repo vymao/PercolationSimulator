cdb04de861fc454cd907631012c64dfdc5237622
Victor <victor.mao@berkeley.edu>
2019-05-25 16:18:55 -0700

    hw2 changes

--- hw2/Percolation.java
+++ hw2/Percolation.java
@@ -4,4 +4,96 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private int[][] world;
+    private WeightedQuickUnionUF groupGrid;
+    private WeightedQuickUnionUF waterGrid;
+    private int dimension;
+    private int OpenSites;
+
+    public Percolation(int N) {
+        // The index 0 will represent a closed site
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        world = new int[N][N];
+        groupGrid = new WeightedQuickUnionUF((N * N) + 2);
+        waterGrid = new WeightedQuickUnionUF((N * N) + 2);
+        dimension = N;
+        OpenSites = 0;
+
+        for (int i = 1; i <= N; i += 1) {
+            groupGrid.union(0, i);
+            groupGrid.union(((N * N) + 1), (i + (N - 1) * N));
+            waterGrid.union(0, i);
+        }
+    }
+
+    public void open(int row, int col) {
+        if (row > dimension || col > dimension) {
+            throw new IllegalArgumentException();
+        }
+
+        OpenSites += 1;
+        world[row][col] = 1;
+        int n1 = col + 1 + (dimension * row);
+
+        try {
+            if (isOpen((row - 1), col)) {
+                int n2 = (col + 1) + (dimension * (row - 1));
+                groupGrid.union(n1, n2);
+                waterGrid.union(n1, n2);
+            }
+        } catch (Exception a) {}
+        try {
+            if (isOpen((row + 1), col)) {
+                int n2 = (col + 1) + (dimension * (row + 1));
+                groupGrid.union(n1, n2);
+                waterGrid.union(n1, n2);
+            }
+        } catch (Exception a) {}
+        try {
+            if (isOpen(row, (col - 1))) {
+                int n2 = (col) + (dimension * row);
+                groupGrid.union(n1, n2);
+                waterGrid.union(n1, n2);
+            }
+        } catch (Exception a) {}
+        try {
+            if (isOpen(row, (col + 1))) {
+                int n2 = (col + 2) + (dimension * row);
+                groupGrid.union(n1, n2);
+                waterGrid.union(n1, n2);
+            }
+        } catch (Exception a) {}
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row > dimension || col > dimension) {
+            throw new IllegalArgumentException();
+        }
+        return world[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row > dimension || col > dimension) {
+            throw new IllegalArgumentException();
+        }
+        if (isOpen(row, col)) {
+            int n1 = col + 1 + (dimension * row);
+            return waterGrid.connected(0, n1);
+        } else {
+            return false;
+        }
+    }
+
+    public int numberOfOpenSites() {
+        return OpenSites;
+    }
+
+    public boolean percolates() {
+        return groupGrid.connected(0, ((dimension * dimension) + 1));
+    }
+
+    public static void main(String[] args) {}
+
 }
---
+++ hw2/Random.java
@@ -0,0 +1,10 @@
+package hw2;
+
+public class Random {
+    public static void main(String[] args) {
+        int hi[][] = new int[2][2];
+        hi[0][0] = 1;
+        hi[1][1] = 2;
+
+    }
+}
